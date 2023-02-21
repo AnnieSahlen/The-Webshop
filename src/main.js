889b5e3f344107f92841d320bd7c8ffd017c64e9
Annie Sahlén <annieimsahlen@gmail.com>
2023-02-21 14:02:47 +0100

    Install bootstrap package

--- src/main.js
+++ src/main.js
@@ -3,5 +3,6 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import bootstrap from './node_modules/bootstrap'
 
-createApp(App).use(router).use(store).mount('#app')
+createApp(App).use(router).use(store).use(bootstrap).mount('#app')
