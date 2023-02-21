f944a8f893db9e555fc17732cd86b4123a5e358b
Jennifer Klang <klang.jennifer@gmail.com>
2023-02-21 15:05:12 +0100

    Add pictures

--- src/main.js
+++ src/main.js
@@ -4,13 +4,4 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-import { OhVueIcon, addIcons } from 'oh-vue-icons'
-import { FaFlag, RiZhihuFill } from 'oh-vue-icons/icons'
-
-addIcons(FaFlag, RiZhihuFill)
-
 createApp(App).use(router).use(store).mount('#app')
-
-const app = createApp(App)
-app.componenent('v-icon', OhVueIcon)
-// Ovanstående två rader är för bootstrap. Se https://oh-vue-icons.js.org/docs. Kan jag lägga in dessa två rade i rad 12?
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,31 @@
-<template><h1>Hem</h1></template>
-<img alt="" src="/assets/woman.jpg" />
-<img alt="" src="/assets/men.jpg" />
-<img alt="" src="/assets/watch.jpg" />
+<style>
+  .homePictures img {
+    display: flex;
+    width: 100%;
+    flex-wrap: wrap;
+  }
+  @media (max-width: 375px) {
+    .homePictures {
+      width: 100%;
+      position: absolute;
+      right: 0;
+      bottom: 1;
+      z-index: -1;
+    }
+  }
+  @media (min-width: 375px) {
+    .homePictures {
+      flex-wrap: wrap;
+      width: 50%;
+    }
+  }
+</style>
+
+<template>
+  <h1>Hem</h1>
+  <div class="homePictures">
+    <img alt="" src="/assets/woman.jpg" />
+    <img alt="" src="/assets/men.jpg" />
+    <img alt="" src="/assets/watch.jpg" />
+  </div>
+</template>
