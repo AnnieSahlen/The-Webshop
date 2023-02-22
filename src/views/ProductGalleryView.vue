7374820f4a104303cad41cd1f1e59c33b4c70aa3
Viktor Mattsson <viktor.a.mattsson@gmail.com>
2023-02-22 14:18:56 +0100

    fix fetch

--- src/views/ProductGalleryView.vue
+++ src/views/ProductGalleryView.vue
@@ -1,23 +1,35 @@
 <script>
   export default {
     created() {
+      // fetch('https://avancera.app/cities/')
       fetch('https://dummyjson.com/products/category/womens-dresses')
         .then((response) => response.json())
         .then((result) => {
-          this.products = result
+          this.products = result.products
         })
     },
     data() {
-      return { products: null }
+      return {
+        products: null
+      }
     }
   }
 </script>
-<style></style>
+<style>
+  img {
+    width: 50%;
+    height: 50%;
+  }
+</style>
 <template>
-  <div calss="container-fluid">
+  <div class="container-fluid">
     <div class="row">
       <div class="col-6" :key="product.id" v-for="product in products">
-        {{ products }}
+        <ul>
+          <li><img :src="product.images[0]" /></li>
+          <li>{{ product.title }}</li>
+          <li>Price: {{ product.price }}</li>
+        </ul>
       </div>
     </div>
   </div>
