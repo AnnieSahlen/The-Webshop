412c174b814c355cc4270f376dd157551df5fc11
Sandra Jakobsson <sandrajakobsson81@gmail.com>
2023-02-23 15:18:04 +0100

    New footer bootsrap grid

--- src/components/FooterComponent.vue
+++ src/components/FooterComponent.vue
@@ -1,70 +1,38 @@
 <script></script>
 
 <style>
-  #item1 {
-    grid-area: header;
-  }
-  #item2 {
-    grid-area: fb;
-    padding-left: 60px;
-  }
-  #item3 {
-    grid-area: li;
-  }
-  #item4 {
-    grid-area: in;
-    padding-right: 100px;
-  }
-  #item5 {
-    grid-area: pp;
-    padding-left: 100px;
-  }
-  #item6 {
-    grid-area: cc;
-    padding-right: 60px;
-  }
-  #item7 {
-    grid-area: footer;
-  }
-  #item8 {
-    grid-area: em;
-  }
-
-  #grid-container {
-    display: grid;
-    grid-template-areas:
-      'header header header header header header header'
-      'fb li in em em pp cc'
-      'footer footer footer footer footer footer footer';
+  #footer {
     background-color: #a08c5b;
     text-align: center;
-    align-content: end;
-    /* width: 50%; */
-    height: 200px;
-    gap: 1px;
-  }
-
-  #item7 {
-    padding-top: 10px;
-    padding-bottom: 30px;
-    font-size: 25px;
   }
 
   i {
-    font-size: 30px;
+    font-size: 1.3rem;
   }
 </style>
 
 <template>
-  <div id="grid-container">
-    <div id="item1" />
-    <div id="item2"><i class="bi bi-facebook" /></div>
-    <div id="item3"><i class="bi bi-linkedin" /></div>
-    <div id="item4"><i class="bi bi-instagram" /></div>
-    <div id="item5"><i class="bi bi-paypal" /></div>
-    <div id="item6"><i class="bi bi-credit-card-2-back" /></div>
-    <div id="item7">&copy; Copywright</div>
-    <div id="item8" />
-    <div id="item9" />
+  <div class="container-fluid" id="footer">
+    <div class="row" style="padding-top: 50px">
+      <div class="col-1">
+        <i class="bi bi-facebook" style="padding-left: 20px" />
+      </div>
+      <div class="col-1">
+        <i class="bi bi-linkedin" style="padding-left: 20px" />
+      </div>
+      <div class="col-1">
+        <i class="bi bi-instagram" style="padding-left: 20px" />
+      </div>
+      <div class="col-5" />
+      <div class="col-1">
+        <i class="bi bi-paypal" style="padding-left: 20px" />
+      </div>
+      <div class="col-1">
+        <i class="bi bi-credit-card-2-back" style="padding-left: 20px" />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12">&copy; Copywright</div>
+    </div>
   </div>
 </template>
